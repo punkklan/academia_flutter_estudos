81a05f7261e8f5328060b7956b1c3120fda35dbb
Marcio Winter <wintermarcio@gmail.com>
2021-04-15 14:44:16 -0400

    update MediaQuery

--- Flutter/flutter_principais_widgets/lib/app/pages/mediaquery_page.dart
+++ Flutter/flutter_principais_widgets/lib/app/pages/mediaquery_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MediaQueryPage extends StatefulWidget {
   static String routerName = '/media';
@@ -14,10 +15,22 @@ class _MediaQueryPageState extends State<MediaQueryPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Inicializando e configurando o ScreenUtil
+    ScreenUtil.init(
+        BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width,
+          maxHeight: MediaQuery.of(context).size.height,
+        ),
+        // informa aqui o tamanho da tela e orientação que foi utilizada para o desenvolvimento
+        designSize: Size(640, 690),
+        orientation: Orientation.portrait);
+
     return Scaffold(
       appBar: _appBar,
       body: Container(
-        width: MediaQuery.of(context).size.width,
+        // definindo largura utilizando o ScreenUtil
+        width: 640.w,
+        // definindo altura pelo MediaQuery
         height: MediaQuery.of(context).size.height,
         child: Column(
           children: [
@@ -27,10 +40,37 @@ class _MediaQueryPageState extends State<MediaQueryPage> {
               color: Colors.amber,
               child: Column(
                 children: [
-                  Text('Tamanho tela: ${MediaQuery.of(context).size.height}'),
-                  Text('Tamanho appBar: ${_appBar.preferredSize.height}'),
-                  Text('Tamanho padding top: ${MediaQuery.of(context).padding.top}'),
-                  Text('Tamanho padding bottom: ${MediaQuery.of(context).padding.bottom}'),
+                  Text(
+                    'Tamanho tela: ${ScreenUtil().screenHeight}',
+                    style: TextStyle(
+                      //
+                      fontSize: ScreenUtil().setSp(20),
+                    ),
+                  ),
+                  Text(
+                    'Tamanho tela largura: ${MediaQuery.of(context).size.width}',
+                    style: TextStyle(
+                      fontSize: ScreenUtil().setSp(20),
+                    ),
+                  ),
+                  Text(
+                    'Tamanho appBar: ${_appBar.preferredSize.height}',
+                    style: TextStyle(
+                      fontSize: ScreenUtil().setSp(20),
+                    ),
+                  ),
+                  Text(
+                    'Tamanho padding top: ${MediaQuery.of(context).padding.top}',
+                    style: TextStyle(
+                      fontSize: ScreenUtil().setSp(20),
+                    ),
+                  ),
+                  Text(
+                    'Tamanho padding bottom: ${MediaQuery.of(context).padding.bottom}',
+                    style: TextStyle(
+                      fontSize: ScreenUtil().setSp(20),
+                    ),
+                  ),
                 ],
               ),
             ),
