39c237a1bac3041bcd41b57e37e4cd02b674021f
Marcio Winter <52296455+wintermarcio@users.noreply.github.com>
2021-04-19 15:56:28 -0400

    updade
    Adicionado mais uma forma de resolução do exercício

--- Dart Básico/Exercicios/6.dart
+++ Dart Básico/Exercicios/6.dart
@@ -30,3 +30,31 @@ void main() {
   }
   ;
 }
+
+//  Outra forma de resolução do exercício
+
+// void main() {
+//     List paciente = [
+//         'Rodrigo Rahman|35',
+//         'Guilheme Rahman|10',
+//         'João Rahman|17',
+//         'Joaquin Rahman|28',
+//         'Luan Rahman|23',
+//     ];
+  
+//   paciente = only18(paciente);
+//   paciente.forEach((item) => print(item));
+
+// }
+
+// List<dynamic> only18 (List<dynamic> paciente) {
+//     List<String> maioresDeIdade = [];
+//     paciente.forEach((pessoa) {
+//       var pessoaSplit = pessoa.split('|');
+//       var nome = pessoaSplit[0];
+//       var idade = int.tryParse(pessoaSplit[1]) ?? 0;
+//       (idade >= 18) ? maioresDeIdade.add('$nome tem $idade anos.') : (){};
+//     }); 
+//     paciente = maioresDeIdade;  
+//   return paciente;  
+// }
